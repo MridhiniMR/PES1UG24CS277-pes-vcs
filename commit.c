70384c8258565babf2ad186f9ab23c832bd20f62
MridhiniMR <mridhinirathan@gmail.com>
2026-04-21 16:29:17 +0530

    Phase 4: loaded index and created tree

--- commit.c
+++ commit.c
@@ -196,6 +196,11 @@ int head_update(const ObjectID *new_commit) {
 int commit_create(const char *message, ObjectID *commit_id_out) {
     // TODO: Implement commit creation
     // (See Lab Appendix for logical steps)
+    Index index;
+if (index_load(&index) != 0) return -1;
+
+ObjectID tree_id;
+if (tree_from_index(&tree_id) != 0) return -1;
     (void)message; (void)commit_id_out;
     return -1;
 }
